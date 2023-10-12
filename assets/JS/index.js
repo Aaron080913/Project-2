72c92f45204b8bc29ad678bc00f8be95e559bc3b
Aaron080913 <>
2023-10-12 18:24:07 +0000

    java added

---
+++ assets/JS/index.js
@@ -0,0 +1,5 @@
+const buttons = document.getElementsByClassName("controls");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const playerImage = document.getElementById("player-img");
+const computerImage = document.getElementById("computer-img");
