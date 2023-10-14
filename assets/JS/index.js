1c90dfa9374a82cef529ba7c2784e81f106122d1
Aaron080913 <>
2023-10-14 11:34:55 +0000

    array added to dom elements for possible chocies

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -3,3 +3,11 @@ const playerScore = document.getElementById("player-score");
 const computerScore = document.getElementById("computer-score");
 const playerImage = document.getElementById("player-img");
 const computerImage = document.getElementById("computer-img");
+const choices = ["Rock", "Paper", "Scissors"];
+
+//* Add Event Listener for buttons *//
+
+for (let button of buttons) {button.addEventListener("click", function () {
+    let player
+}
+)}
